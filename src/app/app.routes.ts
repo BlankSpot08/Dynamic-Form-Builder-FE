8efe9f5163717e31619ab2befafeffe820fdc6b9
Arvin <arvin@comgtech.com>
2024-01-29 22:40:13 +0800

    added the routes default, list and create

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { FormCreateComponent } from './modules/form-create/form-create.component';
+import { FormListComponent } from './modules/form-list/form-list.component';
+
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'form',
+        pathMatch: 'full'
+    },
+    {
+        path: 'form',
+        children: [
+            {
+                path: '',
+                redirectTo: 'list',
+                pathMatch: 'full'
+            },
+            {
+                path: 'list', component: FormListComponent,
+            },
+            {
+                path: 'create', component: FormCreateComponent
+            }
+        ]
+    }
+];
