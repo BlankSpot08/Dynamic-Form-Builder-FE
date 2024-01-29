ea020a63f681ae4b9819bdc17e181c7f27fed536
Arvin <arvin@comgtech.com>
2024-01-29 22:54:36 +0800

    added form component

---
+++ src/app/shared/components/form/form.component.ts
@@ -0,0 +1,44 @@
+import { Component, Input, Type, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';  
+
+import { FormItemComponent } from '../form-fields/form-item/form-item.component';
+
+import { FormGroup, FormBuilder, Validators, FormArray, FormControl, ReactiveFormsModule, AbstractControl } from '@angular/forms';
+import { FormInput } from '../../../models/form-field/form-input';
+import { FormService } from '../../../services/form/form-service.service';
+
+@Component({
+  selector: 'app-form',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule, FormItemComponent],
+  templateUrl: './form.component.html',
+  styleUrl: './form.component.css'
+})
+
+export class FormComponent {
+  protected myForm!: FormGroup;
+
+  get fields() {
+    return this.myForm.controls['fields'] as FormArray;
+  }
+
+  constructor(private formService: FormService) {}
+
+  ngOnInit() {
+    this.myForm = this.formService.getMyForm();
+  }
+
+  getFieldGroup(field: AbstractControl): FormGroup {
+    return field as FormGroup;
+  }
+
+  addFieldGroup(type: string) {
+    this.formService.addFieldGroup(type);
+    this.myForm = this.formService.getMyForm();
+    console.log(this.fields)
+  }
+
+  onSubmit() {
+    this.formService.onSubmit();
+  }
+}
