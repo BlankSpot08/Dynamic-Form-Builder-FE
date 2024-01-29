877e5f34ce43a79284a25e64ad40dfd6dbdcee3a
Arvin <arvin@comgtech.com>
2024-01-29 22:53:33 +0800

    added api service

---
+++ src/app/models/form-field/form-number.ts
@@ -0,0 +1,7 @@
+import { FormInput } from "./form-input";
+
+class FormNumber extends FormInput {
+    constructor(label: string, input: string, answer: string) {
+        super(label, input, answer);
+    }
+}
