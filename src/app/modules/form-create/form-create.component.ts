9881ef5e14c0a201cabb2fa6a7a05e78fc80c12d
Arvin <arvin@comgtech.com>
2024-01-29 22:44:14 +0800

    added form-create-component

---
+++ src/app/modules/form-create/form-create.component.ts
@@ -0,0 +1,31 @@
+import { Component, ViewChild } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common'; 
+
+import { FormComponent } from '../../shared/components/form/form.component';
+
+import { Form } from '../../models/form';
+import { FormField } from '../../shared/components/form-fields/interface/form-field';
+
+import { FormCreateService } from '../../services/form-create/form-create.service';
+
+@Component({
+  selector: 'app-form-create',
+  standalone: true,
+  imports: [FormsModule, FormComponent, CommonModule],
+  templateUrl: './form-create.component.html',
+  styleUrl: './form-create.component.css'
+})
+export class FormCreateComponent {
+  @ViewChild(FormComponent) formComponent!: FormComponent;
+  
+  constructor(private formCreateService: FormCreateService) { }
+
+  addNewField(type:string) {
+    this.formComponent.addFieldGroup(type);
+  }
+  
+  onSubmit() {
+    this.formComponent.onSubmit();
+  }
+}
