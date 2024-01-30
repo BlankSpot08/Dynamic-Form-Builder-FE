7adb3cd2be60999e3ab3c6ef4c8928183b34f902
Arvin <arvin@comgtech.com>
2024-01-30 22:04:14 +0800

    removed file

--- src/app/models/form-field/form-number.ts
+++
@@ -1,7 +0,0 @@
-import { FormInput } from "./form-input";
-
-class FormNumber extends FormInput {
-    constructor(label: string, input: string, answer: string) {
-        super(label, input, answer);
-    }
-}
