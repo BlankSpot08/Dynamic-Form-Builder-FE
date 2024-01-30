5b6c70bc12f89afd9fe691d0ee9d2ef6c1cdd434
Arvin <arvin@comgtech.com>
2024-01-30 16:22:07 +0800

    added the components for the fill

--- src/app/config/constants.ts
+++ src/app/config/constants.ts
@@ -3,11 +3,23 @@ import { FormCreateNumberComponent } from "../shared/components/form-fields/crea
 import { FormCreateDropdownComponent } from "../shared/components/form-fields/creates/form-create-dropdown/form-create-dropdown.component"
 import { FormCreateTitleComponent } from "../shared/components/form-fields/creates/form-create-title/form-create-title.component"
 
+import { FormViewTitleComponent } from "../shared/components/form-fields/show/form-view-title/form-view-title.component"
+
+import { FormFillTextComponent } from "../shared/components/form-fields/fill-up/form-fill-text/form-fill-text.component"
+import { FormFillNumberComponent } from "../shared/components/form-fields/fill-up/form-fill-number/form-fill-number.component"
+import { FormFillDropdownComponent } from "../shared/components/form-fields/fill-up/form-fill-dropdown/form-fill-dropdown.component"
+
 export const FORM_INPUTS: any = {
     "title": FormCreateTitleComponent,
     "dropdown" : FormCreateDropdownComponent, 
     "number": FormCreateNumberComponent,
-    "text": FormCreateTextComponent
+    "text": FormCreateTextComponent,
+
+    "viewTitle": FormViewTitleComponent,
+    
+    "filltext": FormFillTextComponent,
+    "fillnumber": FormFillNumberComponent,
+    "filldropdown": FormFillDropdownComponent
 }
 
 export const INPUT_MULTIPLE: string[] = [
