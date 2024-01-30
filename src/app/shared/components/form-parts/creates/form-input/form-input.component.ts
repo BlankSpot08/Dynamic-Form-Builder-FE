f17e7484cdcee06524f72eb45a789d7cd5504b19
Arvin <arvin@comgtech.com>
2024-01-30 16:38:59 +0800

    added form input component

---
+++ src/app/shared/components/form-parts/creates/form-input/form-input.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormControl, FormGroup } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormService } from '../../../../../services/form/form-service.service';
+
+
+@Component({
+  selector: 'app-form-input',
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule],
+  templateUrl: './form-input.component.html',
+  styleUrl: './form-input.component.css'
+})
+export class FormInputComponent {
+  @Input() inputString: string = 'inputPlaceholder';
+  @Input() customHolder: string = 'default - Enter your input here...' ;
+  @Input() customType: string = 'text';
+  @Input() group!: FormGroup;
+
+  ngOnInit() {}
+}
